bd825d437810458362e9a68c8770c4b8c192c503
gabrielajaen <gabriela.jaen10@gmail.com>
2024-06-30 16:31:43 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PedidosRestaurante',
+  webDir: 'www'
+};
+
+export default config;
